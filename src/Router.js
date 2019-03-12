a9265c3bba8c37c501d0ec5e998ec9431e1196a2
Kim Sanghyeok <skyee23@gmail.com>
2019-03-13 00:58:25 +0900

    store 구독

--- src/Router.js
+++ src/Router.js
@@ -2,10 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import { Provider } from 'react-redux';
+import { store } from './modules/redux';
+
 const Router = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 export default Router;
