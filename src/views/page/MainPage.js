7b0029a430f1f556f948146e2d2643494d93e752
Kim Sanghyeok <skyee23@gmail.com>
2019-03-13 23:06:36 +0900

    불필요한 코드 제거 및 주석

--- src/views/page/MainPage.js
+++ src/views/page/MainPage.js
@@ -4,39 +4,11 @@
 import React, { Fragment, Component } from "react";
 
 import '../../styles/mainpage.css'
-//---------------------------
-// another css in js (학습용)
-//---------------------------
-// const styleSheet = {
-//   container: {
-//     position: "absolute",
-//     top: "50%",
-//     left: "50%",
-//     width: "300px",
-//     height: "300px",
-//     margin: "-150px 0 0 -150px"
-//   },
-//   box: {
-//     display: "table-cell",
-//     width: "inherit",
-//     height: "inherit",
-//     textAlign: "center",
-//     verticalAlign: "middle",
-//     cursor: "pointer"
-//   },
-//   imageContainer: {
-//     width: "300px"
-//   }
-// };
 
 export default class MainPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-  }
-
-  componentDidMount() {
-    console.log(this.props);
   }
 
   goToChart = () => {
